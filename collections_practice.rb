3cdefc96ca71ed655d4fc39269ae2941f3b18dbd
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-28 11:41:46 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,4 +11,8 @@ def sort_array_char_count(array)
 end
 
 def swap_elements_from_to(array, index, destination_index)
-    array.sort_by{ |position[1], position[2]| position[2] <=> position[1] }
+    array.sort do |position[1], position[2]|
+      position[2] <=> position[1] 
+    end
+end
+
