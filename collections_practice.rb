80eed4a36304e06bafaaa1542122b5ba03d4a947
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-28 13:08:00 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -10,23 +10,10 @@ def sort_array_char_count(array)
   array.sort_by{ |word| word.length }
 end
 
-def swap_elements(array)
-  array[1], array[2] = array[2], array[1]
-  array
-end
-
-def reverse_array(array)
-  array.reverse
-end
-
 def swap_elements_from_to(array, index, destination_index)
     array.sort do |position[1], position[2]|
       position[2] <=> position[1] 
     end
-end
-
-def reverse_array(array)
-  array.reverse
 end
 
 def reverse_array(array)
@@ -93,7 +80,7 @@ def sum_array(array)
     # array.reduce(:+)
 
    #using inject method (short)
-     array.inject(:+)
+    # array.inject(:+)
 
   # using inject method (long)
      # array.inject do |sum,x|
