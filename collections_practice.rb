8cf61f448fe4f82638e6675d5dd9f68e32dc2b24
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-10-28 13:51:13 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,8 +4,10 @@ end
 
 sort_array_asc([25, 7, 1]) #[1,7,25]
 
-def sort_array_desc (arary)
-  array.sort { |a,b| b <=> a}
+def sort_array_desc(array)
+  array.sort do | left, right|
+    right <=> left
+  end
 end
 
 sort_array_desc([25, 7, 14]) #[25, 14, 7]
